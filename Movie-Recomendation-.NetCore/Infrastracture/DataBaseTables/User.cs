1f1dec1a0386140f272429f1dd3a62f62e1d7706
ArturDom <arturdomasik1@gmail.com>
2019-12-25 21:44:28 +0100

    infrastructure - data base table

---
+++ Movie-Recomendation-.NetCore/Infrastracture/DataBaseTables/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.DataBaseTables
+{
+    public class User
+    {
+        public int UserID { get; set; }
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int Age {get; set;}
+        public string Nationality { get; set; }
+    }
+}
