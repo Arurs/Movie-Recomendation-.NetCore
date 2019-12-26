46d1de66f596b44e2aa2f92f2f8d836078f35492
ArturDom <arturdomasik1@gmail.com>
2019-12-26 13:29:25 +0100

    data base service module

---
+++ Movie-Recomendation-.NetCore/DataBaseService/DataBaseServiceModule.cs
@@ -0,0 +1,24 @@
+﻿using DataBaseService.Repository;
+using Infrastructure.Repository;
+using Prism.Ioc;
+using Prism.Modularity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBaseService
+{
+    public class DataBaseServiceModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.Register<IMovieRepository, MovieRepository>();
+            //throw new NotImplementedException();
+        }
+    }
+}
