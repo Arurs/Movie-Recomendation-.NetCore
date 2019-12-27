f16f2a92989a265d613b08d7c322e7bff358c59f
ArturDom <arturdomasik1@gmail.com>
2019-12-27 18:15:08 +0100

    user repository

---
+++ Movie-Recomendation-.NetCore/DataBaseService/Repository/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using Infrastructure.DataBaseTables;
+using Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBaseService.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        public void Add(User user)
+        {
+            using (var db = new MovieRecomendationContext())
+            {
+                db.Add(user);
+                db.SaveChanges();
+            }
+        }
+
+        public User Get(int id)
+        {
+            using (var db = new MovieRecomendationContext())
+            {
+                return db.Users.Find(id);
+            }
+        }
+
+        public void Update(User user)
+        {
+            using (var db = new MovieRecomendationContext())
+            {
+                db.Update(user);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Movie-Recomendation-.NetCore/Infrastracture/Repository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.DataBaseTables;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repository
+{
+    public interface IUserRepository
+    {
+        void Add(User user);
+        User Get(int id);
+        void Update(User movie);
+    }
+}
