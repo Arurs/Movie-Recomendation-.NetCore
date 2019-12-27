cf2e8408f55cd5feb5c5c495c3be53469866f30d
ArturDom <arturdomasik1@gmail.com>
2019-12-27 12:38:11 +0100

    movie recomendation - base project

---
+++ Movie-Recomendation-.NetCore/movieRecomendation/App.xaml.cs
@@ -0,0 +1,67 @@
+﻿using CommonServiceLocator;
+using DataBaseService;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace movieRecomendation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+
+        protected override Window CreateShell()
+        {
+            return ServiceLocator.Current.GetInstance<MainWindow>();
+        }
+
+        protected override void InitializeShell(Window shell)
+        {
+
+            base.InitializeShell(shell);
+            shell.Show();
+
+        }
+
+        protected override void RegisterTypes(
+            IContainerRegistry containerRegistry
+        )
+        {
+
+        }
+
+        protected override void ConfigureModuleCatalog(
+            IModuleCatalog moduleCatalog
+        )
+        {
+            base.ConfigureModuleCatalog(moduleCatalog);
+            moduleCatalog.AddModule<DataBaseServiceModule>();
+        }
+
+        //protected override void ConfigureRegionAdapterMappings(
+        //    RegionAdapterMappings regionAdapterMappings
+        //)
+        //{
+        //    base.ConfigureRegionAdapterMappings(regionAdapterMappings);
+
+        //    regionAdapterMappings.RegisterMapping(
+        //        typeof(Ribbon),
+        //        Container.Resolve<RibbonRegionAdapter>()
+        //    );
+
+        //    regionAdapterMappings.RegisterMapping(
+        //        typeof(StackPanel),
+        //        Container.Resolve<StackPanelRegionAdapter>()
+        //    );
+        //}
+    }
+}
