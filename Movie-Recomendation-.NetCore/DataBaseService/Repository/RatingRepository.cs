5f89250f9e2dab6f328429018793e0b8c5cf9b9f
ArturDom <arturdomasik1@gmail.com>
2019-12-27 18:21:46 +0100

    rating repository

---
+++ Movie-Recomendation-.NetCore/DataBaseService/Repository/RatingRepository.cs
@@ -0,0 +1,37 @@
+﻿using Infrastructure.DataBaseTables;
+using Infrastructure.Repository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBaseService.Repository
+{
+    public class RatingRepository : IRatingRepository
+    {
+        public void Add(UserMovieRating rating)
+        {
+            using (var db = new MovieRecomendationContext())
+            {
+                db.Add(rating);
+                db.SaveChanges();
+            }
+        }
+
+        public UserMovieRating Get(int id)
+        {
+            using (var db = new MovieRecomendationContext())
+            {
+                return db.UserMovieRatings.Find(id);
+            }
+        }
+
+        public void Update(UserMovieRating rating)
+        {
+            using (var db = new MovieRecomendationContext())
+            {
+                db.Add(rating);
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Movie-Recomendation-.NetCore/Infrastracture/Repository/IRatingRepository.cs
@@ -0,0 +1,14 @@
+﻿using Infrastructure.DataBaseTables;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Infrastructure.Repository
+{
+    public interface IRatingRepository
+    {
+        void Add(UserMovieRating rating);
+        UserMovieRating Get(int id);
+        void Update(UserMovieRating rating);
+    }
+}
